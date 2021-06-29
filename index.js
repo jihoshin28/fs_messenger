c89ff5386658fabd6c9aa3cb4625378427fb67a5
Allen <jihoshin.28@gmail.com>
2021-06-29 15:51:03 -0700

    inital heroku commit

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express()
+const http = require ('http')
+const server = http.createServer(app)
+const { Server } = require ('socket.io')
+const io = new Server(server)
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+    res.sendFile(__dirname + '/templates/index.html')
 })
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('A user connected')
+
+    socket.on('chat message', (msg) => {
+        console.log('chat message', msg)
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+})
+
+server.listen(3000, () => {
     console.log("Listening on Port 3000")
 })
 

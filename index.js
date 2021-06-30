c23df06d5491cfe4cbe82056fc314fbaed21327e
Allen <jihoshin.28@gmail.com>
2021-06-29 21:28:02 -0700

    working chat

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const express = require('express')
 const app = express()
+const routes = require('./routes')
+const cors = require('cors')
+app.use(routes)
+app.use(cors())
+
+const fs = require('fs')
+const path = require('path')
 const http = require ('http')
 const server = http.createServer(app)
 const { Server } = require ('socket.io')
-const io = new Server(server)
+const io = new Server(server, {path: '/chat-server/', cors: {origin: '*'}})
 const port = 3000 | process.env.PORT
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/templates/index.html')
-})
+let messages = []
 
 io.on('connection', (socket) => {
+    io.socketsJoin('room1')
+    console.log(io.engine.clientsCount)
     console.log('A user connected')
-
+    
+    socket.data.username = "allen"
+    console.log(socket.rooms, socket.data)
+    apiConnect(socket)
+    // fs.writeFile(path.join(__dirname + '/chats' + 'newchat.txt'), (err) => {
+    //     if(err) {
+    //         throw err
+    //     }
+    //     console.log("File written")
+    // })      
+    
     socket.on('chat message', (msg) => {
         console.log('chat message', msg)
-        io.emit('chat message', msg);
+        messages.push(msg)
+        let messageObj = {
+            messages: messages, 
+            id: socket.id
+        }
+        console.log(messages)
+        io.emit('chat message', messageObj);
     });
 
     socket.on('disconnect', () => {
@@ -23,6 +46,12 @@ io.on('connection', (socket) => {
     });
 })
 
+let apiConnect = (socket) => {
+    let details = {}
+    details.date = new Date()
+    
+    socket.emit('api connect', details)
+}
 server.listen(port, () => {
     console.log(`Listening on Port ${port}`)
 })

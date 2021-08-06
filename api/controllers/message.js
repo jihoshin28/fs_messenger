90de34bc15e0053714f050843493d5c9eefb043f
Allen <jihoshin.28@gmail.com>
2021-08-06 15:02:13 -0700

    update and delete messages

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -14,12 +14,23 @@ const getChatMessages = async(req, res) => {
     })
 }
 
-const updateChatMessage = () => {
+const updateChatMessage = async(req, res) => {
+    await Chat.updateOne({'_id': req.params.id}, req.body.newMesage,  (err) => {
+        if(err) {
+            return res.send(err).status(500)
+        }
+    })
+    return res.send(`Updated message with id${req.params.id}`)
+}   
 
-}
-
-const deleteChatMessage = () => {
-
+const deleteChatMessage = async(req, res) => {
+    await Chat.deleteOne({'_id': req.params.id}, (err) => {
+        if(err) {
+            return res.send(err).status(500)
+        }
+    })
+    return res.send(`Deleted message with id${req.params.id}`)
+    
 }
 
 module.exports = {

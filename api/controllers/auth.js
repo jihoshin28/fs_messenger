f3b3bb36b838a710bd6a8d3f4e50610f53cf9d3a
Allen <jihoshin.28@gmail.com>
2021-08-31 19:21:51 -0700

    all login errors

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -7,39 +7,41 @@ dotenv.config()
 const login = async(req, res) => {
 
     await User.find({email: req.body.email}, (err, user) => {
-        console.log(req.body.password)
+    
         if(!req.body.password){
-            console.log('hit')
             return res.json({
                 success: false, 
                 message: "Please enter a password"
             }).status(500)
         } else {
-            if(err) {
+            
+            if(user.length < 1) {
+                console.log('error')
                 return res.json({
                     success: false,
                     message: "No user matches this email"
                 }).status(500)
+            } else {
+                if(req.body.password === user[0].password){
+                    const token = jwt.sign({
+                        username: req.body.username
+                    },
+                    process.env.SECRET)
+                    console.log(token, 'token')
+                    return res.json({
+                        success: true, 
+                        message: "Authorization successful!",
+                        user,
+                        token
+                    })
+                } else {
+                    return res.json({
+                        success: false,
+                        message: "Password is incorrect!"
+                    })
+                }
             }
     
-            if(req.body.password === user[0].password){
-                const token = jwt.sign({
-                    username: req.body.username
-                },
-                process.env.SECRET)
-                console.log(token, 'token')
-                return res.json({
-                    success: true, 
-                    message: "Authorization successful!",
-                    user,
-                    token
-                })
-            } else {
-                return res.json({
-                    success: false,
-                    message: "Passwords do not match!"
-                })
-            }
 
         }
       
